e6ef9719c2a4f05bf8604f6cdfef7222c1895b03
Tuscanny Polk <tuscannypolk@gmail.com>
2021-11-22 17:55:27 -0500

    finished/ working

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 //variables
+const inputBox = document.getElementById('mainInput');
+const addToDoButton = document.getElementById('addToDoButton');
+const clearToDoButton = document.getElementById('clearToDoButton');
+let todoList = document.getElementById('toDoList')
+
 
 
 
 
 //functions
 
+const todoHandler = () => {
 
+let todo = inputBox.value;
+let newTodo = document.createElement('li');
+newTodo.textContent= todo; 
+todoList.appendChild(newTodo);
+}
 
+const clearToDoHandler = () => {
+ while (todoList.hasChildNodes()){ 
+
+todoList.removeChild(todoList.firstChild)
+ } 
+
+}
 
 //events
+
+addToDoButton.addEventListener('click',todoHandler);
+clearToDoButton.addEventListener('click',clearToDoHandler);
